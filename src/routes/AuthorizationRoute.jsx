4ce26bc3fce12d831345e5da6fce8836bc00a4dd
kyky <kyle.vuanh@gmail.com>
2025-04-29 11:39:27 +0700

    [CREATE] AuthorizationRoute

--- src/routes/AuthorizationRoute.jsx
+++ src/routes/AuthorizationRoute.jsx
@@ -1,3 +1,8 @@
-export default function AuthorizationRoute() {
-    return 'AuthorizationRoute';
+import { Navigate } from 'react-router-dom';
+import GetAttribute from '../utils/GetAttribute';
+
+export default function AuthorizationRoute({ role, children }) {
+    const currentRole = GetAttribute('role');
+    if (!role.includes(currentRole)) return <Navigate to='/' replace />;
+    return children;
 }
