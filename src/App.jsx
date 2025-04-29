f33c0500804878061c9dee2fd2265d74946171c9
kyky <kyle.vuanh@gmail.com>
2025-04-29 09:35:05 +0700

    [CREATE] AppRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import AuthenticationRoute from './routes/AuthenticationRoute';
+import LoginPage from './features/auth/LoginPage';
+import MainRoute from './routes/MainRoute';
 import './App.css';
 
 export default function App() {
-  return 'Hi';
+  return (
+    <Router>
+      <div className='h-screen px-4 py-4 overflow-hidden'>
+        <Routes>
+          <Route
+            path='/login'
+            element={
+              <AuthenticationRoute>
+                <LoginPage />
+              </AuthenticationRoute>
+            }
+          />
+          <Route
+            path='/*'
+            element={
+              <AuthenticationRoute>
+                <MainRoute />
+              </AuthenticationRoute >
+            }
+          />
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
