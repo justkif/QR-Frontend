36de0a420196507525f0e57d253acdfd586e0f3c
kyky <kyle.vuanh@gmail.com>
2025-05-04 17:00:08 +0700

    [CREATE] AdminRoute

--- src/routes/AdminRoute.jsx
+++ src/routes/AdminRoute.jsx
@@ -1,3 +1,16 @@
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import AdminPage from '../features/admin/AdminPage';
+import UserPage from '../features/user/UserPage';
+
 export default function AdminRoute() {
-    return 'AdminRoute';
+    return (
+        <div className='h-screen'>
+            <Routes>
+                <Route index element={<AdminPage />} />
+                <Route path='user' element={<UserPage />} />
+                <Route path='*' element={<Navigate to='/admin' replace />} />
+            </Routes>
+            <Outlet />
+        </div>
+    );
 }
